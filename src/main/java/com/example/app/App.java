1a5a1d2e8eda477c8a5b40ba13a6996aad1ae67a
Kavish Rizvi <rizvikavish205.com>
2022-06-21 02:12:55 +0530

    FIrst Commit

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+import com.esri.arcgisruntime.mapping.view.MapView;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    private Macd c:/
+    pView mapView;
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
